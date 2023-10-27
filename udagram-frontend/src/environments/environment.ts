be0249b0b422715e252ec4acb0614672399ff8f3
quyendd7 <quyendd7@fpt.com>
2023-10-27 14:16:23 +0700

    commit

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a7d55cbc3c4374aec8e869785490f823-1747463148.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
